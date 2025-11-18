5d38e8aa35936a78fe7e3ccc2fbe355ca5c56059
anderthetable <andertodlu@gmail.com>
2025-11-18 14:53:25 +0100

    Improvments to commit reveal strategy

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ import { aliceAddress, alicePkh, alicePriv, aliceTokenAddress, bobAddress, bobPk
 import BattleCashArenaArtifact from './BattleCashArena.json' with {type: 'json'}
 import BattleCashManagerArtifact from './BattleCashManager.json' with {type: 'json'}
 
-function calculateWinner(utxo1, utxo2, championId1, championId2, address1, address2){
+function calculateWinner(utxo1, utxo2, championId1, championId2, address1, address2, nonce1, nonce2){
 
-    const finalHashBytes = Array.from(utils.sha256(new Uint8Array([...hexToBin(utxo1.txid).reverse(), ...hexToBin(utxo2.txid).reverse()])));
+    const finalHashBytes = Array.from(utils.sha256(new Uint8Array([...nonce1, ...hexToBin(utxo1.txid).reverse(), ...hexToBin(utxo2.txid).reverse(), ...nonce2])));
+
+    console.log('nonce1:', nonce1);
+    console.log('nonce2:', nonce2);
 
     let input2Strength = 10 * (((finalHashBytes[31] & 0x7f) % 10) + 1)
     let input2Life = 10 * (((finalHashBytes[30] & 0x7f) % 10) + 1)
@@ -26,21 +29,23 @@ function calculateWinner(utxo1, utxo2, championId1, championId2, address1, addre
         if (utxo2.token.category === championId2) input3Life *= 3;
     }
 
+    console.log(binToHex(finalHashBytes))
+
     return input2Life > input3Strength ? address1 : address2;
 }
 
-const BattleCashArenaBytecode = '5479009c63c3539d00ce00879100ce827701209c9a00cf0087919a6951ce827701219c51ce01207f75bc537a879a51ce01207f7751879a51cf020000879a6951ce827751ce01207f75bc51ce01207f77bc51cf52ce008752c602e803a29a6900cd51c78800d200cf8800cc00c69d51d20051805a7a51807e5a7a7e8851cd51c78851cc51c69d006576d378d09d76d178ce88768b777652a26652c652c602e803a063c4539d52cc52c602e803949d52cd52c78852d252cf8867c4529d686d6d6d6d6d5167547a519d52c853c87ea851cebc537a885a78011f7f77017f84815a978b955a5279011f7f75011e7f77017f84815a978b955a5379011e7f75011d7f77017f84815a978b955a547a011d7f75011c7f77017f84815a978b9552cebc56798752cebc5879879b6353795295547a757c6b7c6b7c6c6c52795295537a757c6b7c6c52cebc5679876353795395547a757c6b7c6b7c6c6c6852cebc5779876352795395537a757c6b7c6c686853cebc56798753cebc5879879b637852957b757c7652957753cebc567987637853957b757c6853cebc577987637653957768687b7ba06351cd0376a91454797e0288ac7e886751cd53c7886852cd0376a91454797e0288ac7e88c0d2c0cf88c0d3c0d09dc0d1c0ce88c0ccc0c69d006d6d6d5168'
+const BattleCashArenaBytecode = '5479009c63c3539d00ce00879100ce827701209c9a00cf0087919a6951ce827701219c51ce01207f75bc537a879a51ce01207f7751879a51cf020000879a6952ce008752c602e803a29a6900cd51c78800d200cf8800cc00c69d51d2005180567a51807e567a7e8851cd51c78851cc51c69d006576d378d09d76d178ce8876cc78c69d768b777652a26652c602e803a063c4539d52cc52c602e803949d52cd52c78852d252cf8867c4529d686d6d755167547a519dc3559d00ce827701219c00ce01207f75bc5379879a00ce01207f7751879a00cf527f75517f77010087919a6951cebc7b8751ce827701209c9a51cf00879a6952ce00879152ce827701209c9a52cf0087919a6953ce00879153ce827701209c9a53cf0087919a6954ce008754c602e803a29a69006576d2788bcf8876d3788bd09d76d1788bce8876d1788bce5279cc53798bc69d52798b537a757c6b7c6c6d7653a2665479518052c87e53c87e00cf527f75517f777ea800cf527f7752c8577a518052c87e53c87e00cf527f75517f777e5a5479011f7f77017f84815a978b955a5579011f7f75011e7f77017f84815a978b955a5679011e7f75011d7f77017f84815a978b955a577a011d7f75011c7f77017f84815a978b9552cebc5a798752cebc5c79879b6353795295547a757c6b7c6b7c6c6c52795295537a757c6b7c6c52cebc5a79876353795395547a757c6b7c6b7c6c6c6852cebc5b79876352795395537a757c6b7c6c686853cebc5a798753cebc5c79879b637852957b757c7652957753cebc5a7987637853957b757c6853cebc5b7987637653957768687b7ba06300cd0376a91458797e0288ac7e886700cd0376a91400cf527f777e0288ac7e886851cd0376a914587a7e0288ac7e8852cd0376a91400cf527f777e0288ac7e8854c602e8039c63c4539d6753cc54c602e803949d686d6d6d6d5168'
 const BattleCashArenaBytecodeBin = hexToBin(BattleCashArenaBytecode) 
 
 const provider = new MockNetworkProvider()
 
-const aliceUtxo = randomUtxo({satoshis: 1000n, token: randomNFT()})
+let aliceUtxo = randomUtxo({satoshis: 1000n, token: randomNFT()})
 const aliceFee = randomUtxo()
 let bobUtxo = randomUtxo({satoshis: 1000n, token: randomNFT()})
 //bobUtxo.token.nft.commitment = '';
 //bobUtxo.token.nft.capability = 'mutable'
 //const bobUtxo = randomUtxo({satoshis: 1000n, token: randomToken()})
-const bobFee = randomUtxo()
+let bobFee = randomUtxo()
 let contractUtxoPrize = randomUtxo({satoshis: 1000n, token: randomToken()})
 const mintingContractUtxoState = randomUtxo({satoshis: 1000n, token: randomNFT()})
 mintingContractUtxoState.token.category = contractUtxoPrize.token.category
@@ -112,22 +117,47 @@ const tx2 = await new TransactionBuilder({provider})
 
 utxos = await provider.getUtxos(BattleCashArenaContract.address)
 
+console.log('All ', utxos)
+
+ContractUtxoState = utxos.filter((utxo) => utxo.token.category === contractUtxoPrize[0].token.category && utxo.token.nft)
+
+console.log('state  ', ContractUtxoState)
+
+contractUtxoPrize = utxos.filter((utxo) => utxo.token.category === ContractUtxoState[0].token.category && !utxo.token.nft)
+
+bobUtxo = utxos.filter((utxo) => utxo.token.category === bobUtxo[0].token.category)
+
+aliceUtxo = utxos.filter((utxo) => utxo.token.category === aliceUtxo.token.category)
+
+bobFee = await provider.getUtxos(bobAddress)
+
+const nonce1 = 2;
+const nonce2 = 1;
+
+const hashInput = new Uint8Array([
+    nonce1,
+    ...hexToBin(bobUtxo[0].txid).reverse(),
+    ...hexToBin(aliceUtxo[0].txid).reverse(),
+    nonce2
+]);
+
+console.log(binToHex(hashInput))
+
+console.log('Hash result:', binToHex(utils.sha256(hashInput)));
+console.log(bobUtxo)
+console.log(Buffer.from([nonce1, bobUtxo[0].txid.split("").reverse().join(""), bobUtxo[0].txid.split("").reverse().join(""), nonce2]).toString('hex'))
 
 const tx3 = await new TransactionBuilder({provider})
     //.addInput(bobUtxo, bobSignatureTemplate.unlockP2PKH())
-    .addInput(aliceFee, aliceSignatureTemplate.unlockP2PKH())
-    .addInput()
-    .addInput(contractUtxoPrize[0], BattleCashArenaContract.unlock.Battle())
-    .addInput(bobUtxo[0], BattleCashArenaContract.unlock.Battle())
-    .addInput(aliceUtxo, aliceSignatureTemplate.unlockP2PKH())
-    .addOutput({to: aliceAddress, amount: aliceFee.satoshis - 1000n})
-    .addOutput({to: calculateWinner(bobUtxo[0], aliceUtxo, bobUtxo[0].token.category, aliceUtxo.token.category, bobTokenAddress, aliceTokenAddress), amount: contractUtxoPrize[0].satoshis, token: {category: contractUtxoPrize[0].token.category, amount: contractUtxoPrize[0].token.amount}})
+    .addInput(ContractUtxoState[0], BattleCashArenaContract.unlock.Battle(BigInt(2)))
+    .addInput(contractUtxoPrize[0], BattleCashArenaContract.unlock.Battle(BigInt(2)))
+    .addInput(bobUtxo[0], BattleCashArenaContract.unlock.Battle(BigInt(2)))
+    .addInput(aliceUtxo[0], BattleCashArenaContract.unlock.Battle(BigInt(2)))
+    .addInput(bobFee[0], bobSignatureTemplate.unlockP2PKH())
+    .addOutput({to: calculateWinner(bobUtxo[0], aliceUtxo[0], bobUtxo[0].token.category, aliceUtxo[0].token.category, bobTokenAddress, aliceTokenAddress, new Uint8Array([2]), new Uint8Array([1])), amount: contractUtxoPrize[0].satoshis, token: {category: contractUtxoPrize[0].token.category, amount: contractUtxoPrize[0].token.amount}})
     .addOutput({to: bobTokenAddress, amount: bobUtxo[0].satoshis, token: {category: bobUtxo[0].token.category, amount: bobUtxo[0].token.amount, nft: {commitment: bobUtxo[0].token.nft.commitment, capability: bobUtxo[0].token.nft.capability}}})
-    .addOutput({to: aliceTokenAddress, amount: aliceUtxo.satoshis, token: {category: aliceUtxo.token.category, amount: aliceUtxo.token.amount, nft: {commitment: aliceUtxo.token.nft.commitment, capability: aliceUtxo.token.nft.capability}}})
+    .addOutput({to: aliceTokenAddress, amount: aliceUtxo[0].satoshis, token: {category: aliceUtxo[0].token.category, amount: aliceUtxo[0].token.amount, nft: {commitment: aliceUtxo[0].token.nft.commitment, capability: aliceUtxo[0].token.nft.capability}}})
+    .addOutput({to: aliceAddress, amount: bobFee[0].satoshis - 1000n})
     .send()
 
 console.log(tx3)
-
-for (const arg of BattleCashArenaContract.encodedConstructorArgs){
-    console.log(binToHex(arg))
-}
