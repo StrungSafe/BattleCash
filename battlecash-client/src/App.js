08a2e7cfcd1eaf5a84677fc43cb6c755c9fca128
Miguel Molina Jr. <molim134@gmail.com>
2025-11-18 11:13:24 -0500

    Add lobby menu

--- battlecash-client/src/App.js
+++ battlecash-client/src/App.js
@@ -4,8 +4,58 @@ import Arena from './Arena';
 
 import './App.css';
 
+function Lobby({ onChallenge }) {
+  const challengers = [
+    {
+      name: 'Anon1',
+      nft: 'category',
+      imageUrl: 'https://nfts.bch.guru/img/drops/1.png',
+    },
+    {
+      name: 'Anon2',
+      nft: 'category2',
+      imageUrl: 'https://nfts.bch.guru/img/drops/2.png',
+    },
+  ];
+  return (
+    <div style={{
+      height: '100%',
+      width: '66%',
+      // backgroundColor: 'red',
+      overflowY: 'scroll',
+      scrollbarWidth: 'none',
+      display: 'grid',
+      alignContent: 'start',
+      rowGap: '1rem',
+    }}>
+      {
+        challengers.map(c => (
+          <div style={{ display: 'grid', gridAutoFlow: 'column' }}>
+            <div>
+              {c.name}
+            </div>
+            <div>
+              {c.nft}
+            </div>
+            <div>
+              <img src={c.imageUrl} style={{ width: '4rem', height: '4rem' }} />
+            </div>
+            <div>
+              <input type='button' value='Challenge' onClick={onChallenge} />
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
 function App() {
   const [initialized, setInitialized] = useState(false);
+  const [activeBattle, setActiveBattle] = useState(false);
+  const onChallenge = () => {
+    setActiveBattle(true);
+  };
   return (
     <div className='app'>
       <header className='app-header'>
@@ -20,7 +70,12 @@ function App() {
             )
           }
           {
-            initialized && (
+            initialized && !activeBattle && ( 
+              <Lobby onChallenge={onChallenge} />
+            )
+          }
+          {
+            initialized && activeBattle && (
               <Arena />
             )
           }
--- battlecash-client/src/Menu.js
+++ battlecash-client/src/Menu.js
@@ -1,7 +1,17 @@
 function Menu({ startGame }) {
   return (
     <div className='app-menu'>
-      this is the menu
+      <div>
+        <label style={{ marginRight: '1rem' }} htmlFor='network'>
+          Network:
+        </label>
+        <select name='network' id='network'>
+          <option value='mocknet'>Mocknet</option>
+        </select>
+      </div>
+      <div>
+        -- TODO: Wallet Connect --
+      </div>
       <input className='app-menu__start' type='button' onClick={startGame} value='Start' />
     </div>
   )
